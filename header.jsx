05997203de31dec38c4863df406d6d435d9ab021
karthikgandra <karthikgandra1@gmail.com>
2025-05-06 18:16:35 +0530

    Add files via upload

---
+++ header.jsx
@@ -0,0 +1,12 @@
+
+
+export default function header() {
+    return (
+        <header className="header">
+            <img 
+                src="src/troll-face-internet-meme-hakeem-harrie-transparent-removebg-preview (1).png" 
+            />
+            <h1>Meme Generator</h1>
+        </header>
+    )
+}
